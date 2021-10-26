d88df0dccc1ac0173b4002f253de0809dce90add
Shravan <shravanrevanna@gmail.com>
2021-10-27 01:28:30 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/simplenodice/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.simplenodice;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageViewDice;
+    private final Random rng = new Random();
+
+    public void simplenodice(View view){
+        Intent Browserintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://linktr.ee/shravan_revanna"));
+        startActivity(Browserintent);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageViewDice = findViewById(R.id.image_view_dice);
+        imageViewDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+rollDice();
+            }
+        });
+
+    }
+
+    private void rollDice() {
+        int randomNumber = rng.nextInt(6) + 1;
+
+        switch (randomNumber) {
+            case 1:
+                imageViewDice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageViewDice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageViewDice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageViewDice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageViewDice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageViewDice.setImageResource(R.drawable.dice6);
+                break;
+
+        }
+
+}}
